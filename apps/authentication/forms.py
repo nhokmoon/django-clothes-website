0c5b98f897416dc2c178861600821956c3b6e5e3
tuandev <tranminhtuan12an3nh2019@gmail.com>
2023-01-10 16:27:27 +0700

    update

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -3,10 +3,12 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,PasswordResetForm,UserCreationForm
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 class LoginForm(AuthenticationForm):
@@ -59,3 +61,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+
+
--- apps/home/forms.py
+++ apps/home/forms.py
@@ -1,11 +1,52 @@
-from itertools import product
 from django import forms
 
+from apps.home.models import Product
+
+
+from django.core.validators import MinValueValidator, MaxValueValidator,RegexValidator
 
 class ProductForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=255, 
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(255),
+            RegexValidator(
+                regex='^[A-Za-z0-9 ]*$',
+                message='Product name should only contain letters, numbers and spaces.'
+            )
+        ],
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "name",
+                "class": "form-control"
+            }
+        )
+    )
+    weight = forms.DecimalField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ],
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "weight",
+                "class": "form-control"
+            }
+        )
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "description",
+                "class": "form-control",
+                "id":"floatingTextarea2",
+                "style":"height: 130px"
+            }
+        ))
     class Meta:
-        model = product
-        fields = ['name', 'price', 'description']
+        model = Product
+        fields = ['name', 'weight', 'description']
         widgets = {
             'user': forms.HiddenInput()
         }
